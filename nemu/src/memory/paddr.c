b4e230514280bc340e8fc4296281a331a36428b1
tracer-ics2020 <tracer@njuics.org>
2020-09-13 05:20:25 -0400

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 5d320494d2664557aa8a9b07a14bb18b     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  05:20:25 up  1:45,  4 users,  load average: 0.12, 0.20, 0.18

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -15,7 +15,7 @@ IOMap* fetch_mmio_map(paddr_t addr);
 void init_mem() {
 //#ifndef DIFF_TEST
   srand(time(0));
-  uint32_t *p = (uint32_t *)guest_to_host(0);
+  uint32_t *p = (uint32_t *)guest_to_host(0x100000);
   int i;
    for (i = 0; i < PMEM_SIZE / sizeof(p[0]); i ++) {
     p[i] = rand();
