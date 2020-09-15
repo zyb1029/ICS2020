a64f18b985e7bd110eb658c89e3fa42d731dccea
tracer-ics2020 <tracer@njuics.org>
2020-09-15 03:05:02 -0400

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 2465069433064f50aeb69e83226f6c89     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  03:05:02 up  1:51,  4 users,  load average: 0.12, 0.13, 0.16

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -197,7 +197,7 @@ word_t eval(int p, int q) {
 			}
 		}
 		int32_t val1 = eval(p, op -1);
-		int32_t val2 = eval(op + 1, q);
+		int32_t val2 = 0;//eval(op + 1, q);
 		switch (op_type) {
 			case '+': return val1 + val2;
 			case '-': return val1 - val2;
