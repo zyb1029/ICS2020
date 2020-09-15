7586062e8634703d63c17886e45028ec8f4d0fd6
tracer-ics2020 <tracer@njuics.org>
2020-09-15 09:24:19 -0400

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 2465069433064f50aeb69e83226f6c89     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  09:24:19 up  5:26,  5 users,  load average: 0.17, 0.12, 0.14

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,13 +1,38 @@
+#include<stdio.h>
 void init_monitor(int, char *[]);
 void engine_start();
 int is_exit_status_bad();
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
+  
   init_monitor(argc, argv);
 
   /* Start engine. */
   engine_start();
-
+ 
+/*  FILE *fp = fopen("~/ics2020/nemu/tools/gen-expr/.input","r");
+  assert(fp != NULL);
+  uint32_t result;
+  char buff[65536];
+  while (fgets(buff, 10000, fp) != NULL){
+	int len = strlen(buff);
+	buff[len - 1] = 0;
+	fscanf(buff,"%u", &result);
+	int len_result = 0;
+	uint32_t tmp = result;
+	while(tmp) {
+		len_result++;
+		tmp/=10;
+	}
+	bool p = true;
+	if(result == expr(buff + len_result, p)){
+		puts("OK");
+	}
+	else {
+		printf("%u %s\n", result, buff + len_result);
+	}
+	fclose(fp);
+  }*/
   return is_exit_status_bad();
 }
