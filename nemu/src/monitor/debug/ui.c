50e9fe72735fcf4fd8cd3ca2ad49806c70f1803d
tracer-ics2020 <tracer@njuics.org>
2020-09-16 04:18:50 -0400

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 7740b6ced6c4401f9768d29aa61636ba     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  04:18:50 up 47 min,  4 users,  load average: 0.67, 0.29, 0.22

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -11,9 +11,9 @@ void cpu_exec(uint64_t);
 int is_batch_mode();
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
-//static char buff[65536];
+static char buff[65536];
 static char* rl_gets() {
-  static char *line_read = NULL;
+/*  static char *line_read = NULL;
 
   if (line_read) {
     free(line_read);
@@ -27,7 +27,7 @@ static char* rl_gets() {
   }
 
   return line_read;
- /* FILE *fp = fopen("/home/zyb/ics2020/nemu/tools/gen-expr/input","r");
+ */ FILE *fp = fopen("/home/zyb/ics2020/nemu/tools/gen-expr/input","r");
   assert(fp != NULL);
   uint32_t result;
   while(fgets(buff, 10000, fp) != NULL) {
@@ -52,7 +52,7 @@ static char* rl_gets() {
   }
   fclose(fp);
   strcpy(buff, "");
-  return buff;*/
+  return buff;
 }
 
 static int cmd_c(char *args) {
