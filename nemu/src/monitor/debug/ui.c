ee01b4c396bfa42d4de6aa018c34c38db0033ff9
tracer-ics2020 <tracer@njuics.org>
2020-09-24 02:03:39 -0400

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: d53bb832e5584100a366560a5bc7063e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  02:03:40 up  1:01,  3 users,  load average: 0.02, 0.29, 0.51

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -42,7 +42,7 @@ static char* rl_gets() {
 		  printf("%s\n%u\n ",buff + len_result, result);
 	  }
 	  else {
-		  assert(0);
+//		  assert(0);
 		  printf("%u %s\n", result, buff + len_result);
 	  }
 	  
