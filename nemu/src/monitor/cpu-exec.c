10284345efa5ae3ff174d9c9db5c967855756647
tracer-ics2020 <tracer@njuics.org>
2020-09-30 02:41:05 -0400

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 592dc1042ef341fea752296de25ec881     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  02:41:05 up 39 min,  7 users,  load average: 0.39, 0.28, 0.27

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -29,7 +29,6 @@ int is_exit_status_bad() {
   if (nemu_state.state == NEMU_STOP) nemu_state.state = NEMU_QUIT;	
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
-	printf("%d\n", nemu_state.state);
   return !good;
 }
 
