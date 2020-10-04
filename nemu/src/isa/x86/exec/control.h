85a3303d3c43cba12d67c53836882b932b529ac6
tracer-ics2020 <tracer@njuics.org>
2020-10-04 00:09:42 -0400

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: f30245a297bc4f168bee20d10a3a331f     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  00:09:42 up  3:49,  4 users,  load average: 0.23, 0.21, 0.16

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -24,8 +24,8 @@ static inline def_EHelper(jmp_rm) {
 
 static inline def_EHelper(call) {
   // the target address is calculated at the decode stage
-  TODO();
-
+  //TODO();
+  rtl_j(s, s->jmp_pc);
   print_asm("call %x", s->jmp_pc);
 }
 
--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -47,14 +47,15 @@ static inline def_DopHelper(I) {
 /* sign immediate */
 static inline def_DopHelper(SI) {
   assert(op->width == 1 || op->width == 4);
-
+  sword_t simm = instr_fetch(&s->seq_pc, op->width);
+  operand_imm(s, op, load_val, simm, op->width);
   /* TODO: Use instr_fetch() to read `op->width' bytes of memory
    * pointed by 's->seq_pc'. Interpret the result as a signed immediate,
    * and call `operand_imm()` as following.
    *
    operand_imm(s, op, load_val, ???, op->width);
    */
-  TODO();
+ // TODO();
 }
 
 /* I386 manual does not contain this abbreviation.
