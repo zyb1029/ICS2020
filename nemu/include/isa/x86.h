dd40809ad2929c4196ab9c2e7ed254c55bfff62e
tracer-ics2020 <tracer@njuics.org>
2020-10-05 08:42:29 -0400

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 9a62fbdd7171474db943e1532523eb5d     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:42:29 up  2:30,  5 users,  load average: 0.11, 0.08, 0.03

--- nemu/include/isa/x86.h
+++ nemu/include/isa/x86.h
@@ -35,7 +35,20 @@ typedef struct {
 		};
 	};
 	vaddr_t pc;
-
+	union {
+		struct {
+			uint32_t CF	:1;
+			uint32_t	:5;
+            uint32_t ZF :1;
+			uint32_t SF	:1;
+			uint32_t    :1;
+			uint32_t IF :1;
+			uint32_t    :1;
+			uint32_t OF :1;
+			uint32_t    :20;
+		};	
+		uint32_t val;
+	}EFLAGS;
 } x86_CPU_state;
 
 // decode
