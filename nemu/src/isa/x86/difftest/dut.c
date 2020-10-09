b294d825475722e2eb336a95e24f746c53db1f7c
tracer-ics2020 <tracer@njuics.org>
2020-10-09 07:52:05 -0400

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 1ab8991e0bea41438f200e9a7cffcfde     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  07:52:05 up  4:33,  4 users,  load average: 0.15, 0.17, 0.11

--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -4,7 +4,39 @@
 #include "difftest.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  if (cpu.eax != ref_r -> eax) {
+	  printf("eax should be %x\n", ref_r ->eax);
+	  return false;
+  }
+  if (cpu.ecx != ref_r -> ecx) {
+	  printf("ecx should be %x\n", ref_r ->ecx);
+	  return false;
+  }
+  if (cpu.edx != ref_r -> edx) {
+	  printf("edx should be %x\n", ref_r ->edx);
+	  return false;
+  }
+  if (cpu.esp != ref_r -> esp) {
+	  printf("esp should be %x\n", ref_r ->esp);
+	  return false;
+  }
+  if (cpu.ebp != ref_r -> ebp) {
+	  printf("ebp should be %x\n", ref_r ->ebp);
+	  return false;
+  }
+  if (cpu.esi != ref_r -> esi) {
+	  printf("esi should be %x\n", ref_r ->esi);
+	  return false;
+  }
+  if (cpu.edi != ref_r -> edi) {
+	  printf("edi should be %x\n", ref_r ->edi);
+	  return false;
+  }
+  if (cpu.ebx != ref_r -> ebx) {
+	  printf("ebx should be %x\n", ref_r ->ebx);
+	  return false;
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
