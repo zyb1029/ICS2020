ea167898d61bd4215771672b928bf91fe4a2390c
tracer-ics2020 <tracer@njuics.org>
2020-10-09 12:58:27 -0400

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: c084ad7fd3984f7480830236cb294234     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:58:27 up 35 min,  3 users,  load average: 0.26, 0.09, 0.03

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -2,6 +2,14 @@
 
 static inline def_EHelper(add) {
   rtl_add(s, s0, ddest, dsrc1);
+  rtl_update_ZFSF(s, s0, id_dest->width);
+  rtl_is_add_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
+  rtl_set_OF(s, s1);
+  if (id_dest->width != 4) {
+    rtl_andi(s, s0, s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
+  }
+  rtl_is_add_carry(s, s1, s0, dsrc1);
+  rtl_set_CF(s, s1);
   operand_write(s, id_dest, s0);
   print_asm_template2(add);
 }
@@ -19,6 +27,11 @@ static inline void cmp_internal(DecodeExecState *s) {
 
 static inline def_EHelper(sub) {
   rtl_sub(s, s0, ddest, dsrc1);
+  rtl_update_ZFSF(s, s0, id_dest->width);
+  rtl_is_sub_carry(s, s1, ddest, dsrc1);
+  rtl_set_CF(s, s1);
+  rtl_is_sub_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
+  rtl_set_OF(s, s1);
   operand_write(s, id_dest, s0);
   print_asm_template2(sub);
   //TODO();
@@ -35,13 +48,28 @@ static inline def_EHelper(cmp) {
 }
 
 static inline def_EHelper(inc) {
-  rtl_addi(s, s0, ddest, 1);
+  rtl_addi(s, dsrc1, rz, 1);
+  rtl_add(s, s0, ddest, dsrc1);
+  rtl_update_ZFSF(s, s0, id_dest->width);
+  rtl_is_add_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
+  rtl_set_OF(s, s1);
+  if (id_dest->width != 4) {
+    rtl_andi(s, s0, s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
+  }
+  rtl_is_add_carry(s, s1, s0, dsrc1);
+  rtl_set_CF(s, s1);
   operand_write(s, id_dest, s0);
   print_asm_template1(inc);
 }
 
 static inline def_EHelper(dec) {
-  rtl_subi(s, s0, ddest, 1);
+  rtl_addi(s, dsrc1, rz, 1);
+  rtl_sub(s, s0, ddest, dsrc1);
+  rtl_update_ZFSF(s, s0, id_dest->width);
+  rtl_is_sub_carry(s, s1, ddest, dsrc1);
+  rtl_set_CF(s, s1);
+  rtl_is_sub_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
+  rtl_set_OF(s, s1);
   operand_write(s, id_dest, s0);
   print_asm_template1(dec);
 }
