3764d2f592d8161917137526903fdced9c028cfd
tracer-ics2020 <tracer@njuics.org>
2020-10-12 23:57:15 -0400

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: a6e4cae64ea74a22a6038d8f84e70521     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  23:57:15 up  1:26,  3 users,  load average: 0.14, 0.23, 0.18

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -23,8 +23,16 @@ char* strncpy(char* dst, const char* src, size_t n) {
   return NULL;
 }
 
-char* strcat(char* dst, const char* src) {
-  return NULL;
+char* strcat(char* dst, const char* src) { 
+	assert (dst != NULL && src != NULL);
+	int i;
+	for (i = 0; *(dst + i) != '\0'; i++);
+	int j;
+	for (j = 0; *(src + j) != '\0'; j++) {
+		*(dst + i + j) = *(src + j);
+	}
+	*(dst + i + j) = '\0';
+	return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
