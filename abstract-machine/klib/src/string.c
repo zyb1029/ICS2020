771c9a950b7d5533498ffb3b8960690df17eb490
tracer-ics2020 <tracer@njuics.org>
2020-10-13 13:18:10 -0400

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 443d6b098e1046c7ab8df546f8d6346f     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:18:10 up  2:33,  5 users,  load average: 0.19, 0.16, 0.17

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,12 +4,14 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
+  assert(0);
   size_t ans = 0;
   for (int i = 0; s[i] != 0; i++) ans++;
   return ans;
 }
 
 char *strcpy(char* dst,const char* src) {
+  assert(0);
 	assert(dst != NULL && src != NULL);
 	int i;
 	for (i = 0; *(src + i) != '\0'; i++) {
@@ -25,6 +27,7 @@ char* strncpy(char* dst, const char* src, size_t n) {
 }
 
 char* strcat(char* dst, const char* src) { 
+  assert(0);
 	assert (dst != NULL && src != NULL);
 	int i;
 	for (i = 0; *(dst + i) != '\0'; i++);
@@ -37,6 +40,7 @@ char* strcat(char* dst, const char* src) {
 }
 
 int strcmp(const char* s1, const char* s2) {
+  assert(0);
 	assert (s1 != NULL && s2 != NULL);
 	while (*s1 == *s2) {
 		if (*s1 == '\0') {
@@ -54,6 +58,7 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 }
 
 void* memset(void* v,int c,size_t n) {
+  assert(0);
   char *dst = (char *)v;
   for (int i = 0; i < n; i++) {
 	  *(dst + i) = c;	  
@@ -68,6 +73,7 @@ void* memmove(void* dst,const void* src,size_t n) {
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
+  assert(0);
 	unsigned char *s1 = (unsigned char *)out;
 	unsigned char *s2 = (unsigned char *)in;
 	for (int i = 0; i < n; i++) *(s1 + i) = *(s2 + i);
@@ -75,6 +81,7 @@ void* memcpy(void* out, const void* in, size_t n) {
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
+  assert(0);
   if (n == 0) {
 	return 0;	  
   }
