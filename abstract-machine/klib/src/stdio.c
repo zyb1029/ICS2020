2c9792633188ec712baad6634fa54dc01126b8ca
tracer-ics2020 <tracer@njuics.org>
2020-10-13 07:45:36 -0400

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: a6e4cae64ea74a22a6038d8f84e70521     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  07:45:36 up  3:38,  4 users,  load average: 0.51, 0.39, 0.27

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -15,9 +15,66 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   return 0;
 }
 
+int my_atoi(char *dst, int d, int type) {
+	if (type == 1) {
+		char s[20];
+		int len = 0;
+		if (d == 0) {
+			len = 1;
+			*dst = 0 + '0';	
+		}
+        else {
+			while (d) {
+			 s[len++] = d % 10 + '0';	
+			 d = d / 10;	
+			}
+			for (int i = len - 1; i >= 0; i--) {
+				*(dst + len - 1 -i) = *(s + i);
+			}
+		}
+		return len;
+	}	
+	else return 0;
+}
+
 int sprintf(char *out, const char *fmt, ...) {
-  assert(0);
-  return 0;
+  va_list ap;
+  va_start(ap, fmt);
+  int d, len = 0;
+  char *s;
+  while (*fmt != '\0') {
+	switch(*fmt) {
+      case '%':
+		fmt++;
+		switch(*fmt) {
+			case 'd':
+				d = va_arg(ap, int);
+				int tmpd =  my_atoi(out, d, 1);
+				out = out + tmpd; fmt++;
+			    len += tmpd;
+				break;
+			case 's':
+				s = va_arg(ap, char *);
+				strcpy(out, s);
+				int tmps = strlen(out);
+				out = out + tmps; fmt++;
+				len += tmps;
+				break;
+			default:
+				*out = '%'; out++;
+				*out = *fmt; fmt++;
+				len += 2;
+		}
+		break;
+	  default:	
+		*out = *fmt;
+		out++;
+		fmt++;
+		len++;
+	}	  
+  }
+  *out = *fmt;
+  return len;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
