f2c9a560f805d463be01bcf09d4e56c43bfc0537
tracer-ics2020 <tracer@njuics.org>
2020-10-15 22:08:25 -0400

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 3105b2e4a1e744cd90b1785f22b3e8c9     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:08:25 up  4:00,  4 users,  load average: 0.28, 0.27, 0.27

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -14,8 +14,8 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
     struct timeval now;
     gettimeofday(&now, NULL);
     rtc_port_base[0] = now.tv_usec;
-	printf("%ld\n", now.tv_usec);
     rtc_port_base[1] = now.tv_sec;
+	printf("%ld\n", now.tv_sec);
   }
 }
 
