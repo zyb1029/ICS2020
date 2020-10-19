137480f0c60cedfbed0ec9a80506f3b5e05b9ee9
tracer-ics2020 <tracer@njuics.org>
2020-10-19 08:39:39 -0400

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: b7904adf480d4d639704b3b1365068b9     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:39:39 up  2:51,  8 users,  load average: 0.09, 0.10, 0.13

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -15,27 +15,26 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
     .width = W,  .height = H,
     .vmemsz = 0
   };
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (int i = 0; i < 400 * 300; i++)fb[i] = i;
-  outl(SYNC_ADDR, 1); 
+	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+	for (int i = 0; i < 32 *32; i++)fb[i] = i;
+	outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+/*  if (ctl->pixels != NULL) {
+		int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl -> h;
+		uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+		uint32_t * pixels = ctl->pixels;
+		int p = (w < W - x) ? w : W - x;
+		int cp_bytes = sizeof(uint32_t) * p;
+		for (int j = 0; j < h && y + j < H; j++) {
+			memcpy(&fb[(y * h + j) * W + x * w], pixels, cp_bytes);
+			pixels += w;
+		}
+	}*/
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
-  return;
-  if (ctl->pixels != NULL) {
-		int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl -> h;
-		uint32_t * pixel = ctl->pixels;
-		int p = (w < W - x) ? w : W - x;
-		for (int j = 0; j < h && y + j < H; j++) {
-			for (int k = x; k < x + p; k++) {
-//				outl(VGACTL_ADDR , *(pixel + k));
-			}
-			pixel += w;
-		}
-	}
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
