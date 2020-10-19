5fbdd4ef0385f1369ee4bfaf308574c13ce26960
tracer-ics2020 <tracer@njuics.org>
2020-10-19 09:04:41 -0400

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: b7904adf480d4d639704b3b1365068b9     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  09:04:41 up  3:16,  8 users,  load average: 0.38, 0.43, 0.27

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -1,7 +1,6 @@
 #include <am.h>
 #include <nemu.h>
 #include<assert.h>
-#include<stdio.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
@@ -23,7 +22,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->pixels != NULL) {
+/*  if (ctl->pixels != NULL) {
 		int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl -> h;
 		uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
 		uint32_t * pixels = ctl->pixels;
@@ -31,15 +30,13 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 		int cp_bytes = sizeof(uint32_t) * p;
 		for (int j = 0; j < h && y + j < H; j++) {
 			for (int k = 0; cp_bytes; k++) {
-				if ((y + j)*W + x >= 400 * 300)
-					printf("%d %d %d \n", y, j, x);
-	//			assert ((y + j) * W + x < 400 * 300);
-			    fb[(y + j) * W + x -x - y*W - j * W] = *(pixels + k);
+				assert ((y + j) * W + x < 400 * 300);
+				fb[(y + j) * W + x] = *(pixels + k);
 			}
 			//memcpy(&fb[(y * h + j) * W + x * w], pixels, cp_bytes);
-		//	pixels += w;
+			pixels += w;
 		}
-	}
+	}*/
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
