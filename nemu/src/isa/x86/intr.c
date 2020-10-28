867203b9640542cd630eebf9ae25961bc4ad145c
tracer-ics2020 <tracer@njuics.org>
2020-10-28 09:52:47 -0400

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: e257cba8b5a54c828a6f8233a1c5e07f     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  09:52:47 up 12:14, 13 users,  load average: 0.25, 0.24, 0.16

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -11,7 +11,7 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
 	rtl_li(s, s1, ret_addr);
 	rtl_lm(s, s0, s1, NO * 8, 4);
 	vaddr_t Jpc = (*s0) & 0xff;
-	printf("%08x", *s0);
+	printf("%08x", Jpc);
 	rtl_lm(s, s0, s1, NO * 8 + 4, 4);
 	Jpc += (*s0) & (0xff << 16);
   /* TODO: Trigger an interrupt/exception with ``NO''.
