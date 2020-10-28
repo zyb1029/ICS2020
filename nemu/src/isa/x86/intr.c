c1215b86b5c1c6360491f72103f113b1bdf0b2ae
tracer-ics2020 <tracer@njuics.org>
2020-10-28 09:53:18 -0400

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: e257cba8b5a54c828a6f8233a1c5e07f     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  09:53:18 up 12:15, 13 users,  load average: 0.28, 0.25, 0.16

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -10,10 +10,10 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
 	rtl_push(s, s0);
 	rtl_li(s, s1, ret_addr);
 	rtl_lm(s, s0, s1, NO * 8, 4);
-	vaddr_t Jpc = (*s0) & 0xff;
-	printf("%08x", Jpc);
+	vaddr_t Jpc = (*s0) & 0xffff;
 	rtl_lm(s, s0, s1, NO * 8 + 4, 4);
-	Jpc += (*s0) & (0xff << 16);
+	Jpc += (*s0) & (0xffff << 16);
+	printf("%08x", Jpc);
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
