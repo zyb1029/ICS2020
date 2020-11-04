fea636df415bcb15f399de7d09714792269ce89b
tracer-ics2020 <tracer@njuics.org>
2020-11-04 12:50:29 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 700909f9c5be477182453fbc30197d31     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:50:29 up  9:55,  5 users,  load average: 0.02, 0.23, 0.28

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,7 +16,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Phdr *phdr = (Elf_Phdr *)malloc(sizeof(Elf_Phdr) * elf_head.e_phnum);
   ramdisk_read(phdr, 0, sizeof(Elf_Phdr) * elf_head.e_phnum);
   for (int i = 0; i < elf_head.e_phnum; i++){
-	printf("0x%x\n", phdr[i].p_offset);  
+	printf("0x%x\n", phdr[i].p_type);  
 	}
   return 0;
 }
