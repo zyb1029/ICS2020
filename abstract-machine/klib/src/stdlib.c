3ade66fa5d9b8f8d3545eba3553d810b2a917b4a
tracer-ics2020 <tracer@njuics.org>
2020-11-24 19:10:48 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 8f1e420ff95b41f997575732a00f7acc     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  19:10:48 up 7 min,  3 users,  load average: 0.45, 0.37, 0.24

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -32,7 +32,9 @@ int atoi(const char* nptr) {
 static char *addr;
 
 void *malloc(size_t size) {
-
+  #if defined(__ISA_NATIVE__)
+  return;
+  #endif
   if (addr == NULL) addr = (void *)ROUNDUP(heap.start, 8);
   size = (size_t)ROUNDUP(size, 8);
   char *old = addr;
