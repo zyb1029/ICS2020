5cca8a0ebb09bce0dc4f86a902aa75e910ae7d1f
tracer-ics2020 <tracer@njuics.org>
2020-11-26 11:43:55 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 8f1e420ff95b41f997575732a00f7acc     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  11:43:55 up 18:41, 18 users,  load average: 0.16, 0.19, 0.15

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,12 +23,18 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
+  char tep[32];
   bool has_kbd = io_read(AM_INPUT_CONFIG).present;
   if (has_kbd) {
 	  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-	  sprintf(buf, "Got(kbd): %s (%d) %s\n", keyname[ev.keycode], ev.keycode, ev.keydown ? "DOWN" : "UP");
+	  sprintf(tep, "%s %s\n", ev.keydown ? "kd" : "ku", keyname[ev.keycode]);
   }
-  return len;
+  int event_len = strlen(tep);
+  char *buff = (char *)buf;
+  if(event_len > len) event_len = len;
+  for (int i = 0; i < event_len; i++) buff[i] = tep[i];
+  buff[event_len] = '\0';
+  return event_len;
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
