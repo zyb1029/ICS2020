28421204554cc87ef8b54d9b2ef535d23892f491
tracer-ics2020 <tracer@njuics.org>
2020-11-26 09:17:57 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 8f1e420ff95b41f997575732a00f7acc     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  09:17:57 up 16:56, 17 users,  load average: 0.94, 0.48, 0.33

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -7,7 +7,9 @@ int main() {
   int sec = 1;
   while(1) {		  
      gettimeofday(&tv, NULL);
-	 while((tv.tv_sec * 1000000 + tv.tv_usec + 500) / 1000000 < sec);
+	 while((tv.tv_sec * 1000000 + tv.tv_usec + 500) / 1000000 < sec){
+		gettimeofday(&tv, NULL);	 
+	}
 	 if (sec == 1) printf("%d second\n", sec);
 	 else printf("%d seconds\n", sec);
   }
