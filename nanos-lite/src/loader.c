16e83756849de80d6b96f1e7edc76e7d6bfdf6d1
tracer-ics2020 <tracer@njuics.org>
2020-11-25 22:05:44 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 8f1e420ff95b41f997575732a00f7acc     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:05:45 up 10:43,  6 users,  load average: 0.50, 0.27, 0.25

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -29,9 +29,9 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 		printf("%08x %08x\n", elf_head.e_machine, tep);
 //		panic("The ISA counldn't be matched!");	  
 	  }
-	  Elf_Phdr *phdr = (Elf_Phdr *)malloc(sizeof(Elf_Phdr) * elf_head.e_phnum);
+	  uintptr_t addr = elf_head.e_entry;
+/*	  Elf_Phdr *phdr = (Elf_Phdr *)malloc(sizeof(Elf_Phdr) * elf_head.e_phnum);
 	  ramdisk_read(phdr, elf_head.e_phoff, sizeof(Elf_Phdr) * elf_head.e_phnum);
-	 // uintptr_t addr = elf_head.e_entry;
 	  for (int i = 0; i < elf_head.e_phnum; i++){
 		 uint32_t type = phdr[i].p_type; 
 		 uintptr_t VirtAddr = phdr[i].p_vaddr;
@@ -42,8 +42,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 		// printf("%08x %08x %08x %08x %08x %08x\n", fb, type, offset, VirtAddr, FileSiz, Memsiz); 
 		 ramdisk_read(fb, offset, FileSiz);
 		 memset(fb + FileSiz, 0, Memsiz - FileSiz);
-	  }
-	  return 0x3001246;//addr;
+	  }*/
+	  return addr;
    }
    else {
 		return 0;	   	   
