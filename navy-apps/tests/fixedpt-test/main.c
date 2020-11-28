057c40ee233cf2a7f358d0e2e89e52de7bec35e9
tracer-ics2020 <tracer@njuics.org>
2020-11-28 07:22:24 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 8f1e420ff95b41f997575732a00f7acc     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  07:22:24 up 1 day,  8:22, 22 users,  load average: 0.32, 0.22, 0.20

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -2,7 +2,7 @@
 #include <fixedptc.h>
 
 int main() {
-  fixedpt a = fixedpt_rconst(1.2);
+  fixedpt a = fixedpt_rconst(5.5);
   fixedpt b = fixedpt_fromint(10);
   int c = fixedpt_toint(fixedpt_mul(a, b));
   printf("%d\n", c);
