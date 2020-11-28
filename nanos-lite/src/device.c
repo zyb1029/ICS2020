df83cff0b476683aa5a4c3c60762019ef4a7a341
tracer-ics2020 <tracer@njuics.org>
2020-11-28 03:24:00 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 8f1e420ff95b41f997575732a00f7acc     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  03:24:00 up 1 day,  7:24, 23 users,  load average: 0.12, 0.40, 0.41

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -52,11 +52,10 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  int y = offset / io_read(AM_GPU_CONFIG).width;
-  int x = offset - y * io_read(AM_GPU_CONFIG).width;
-  printf("%d %d\n", x, y);
+//  int y = offset / io_read(AM_GPU_CONFIG).width;
+//  int x = offset - y * io_read(AM_GPU_CONFIG).width;
   uint32_t * p = (uint32_t *)buf; 
-  io_write(AM_GPU_FBDRAW, x, y, p, 1, len, true);
+  io_write(AM_GPU_FBDRAW, 0, 0, p, len, 1, true);
   return len;
 }
 
