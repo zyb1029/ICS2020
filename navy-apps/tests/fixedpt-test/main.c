e6a2c7d1c9fe4121b99d6a2e525ba7e9dd97dda6
tracer-ics2020 <tracer@njuics.org>
2020-11-28 07:49:42 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 8f1e420ff95b41f997575732a00f7acc     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  07:49:42 up 1 day,  8:49, 22 users,  load average: 0.45, 0.34, 0.28

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <fixedptc.h>
+#include <math.h>
+#include <assert.h>
+#include <stdlib.h>
 
 int main() {
-  fixedpt a = fixedpt_rconst(21.5);
-  fixedpt b = fixedpt_fromint(10);
-  int c = fixedpt_toint(fixedpt_floor(fixedpt_add(a, b)));
+  int T = 100;
+  while(T--) {
+  int p = rand() % 100, q = rand() % 100 + 1;
+  fixedpt a = fixedpt_rconst(p/q);
+  fixedpt b = fixedpt_fromint(p - q);
+  int c = fixedpt_toint(fixedpt_floor(fixedpt_mul(a, b)));
+  assert(c == floor((p/q) * (p - q)));
   printf("%d\n", c);
+  }
   return 0;
 }
