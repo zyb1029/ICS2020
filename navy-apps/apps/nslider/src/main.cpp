601b85480d3f7256b8376b849c3cadcb6fc9b013
tracer-ics2020 <tracer@njuics.org>
2020-11-30 12:07:45 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 4d072052e06f4130b0dbdab8a218d6f7     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:07:45 up  5:54, 10 users,  load average: 0.69, 0.48, 0.31

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -12,12 +12,8 @@
 //   gg - first page
 
 // number of slides
-const int N = 53;
-// slides path pattern (starts from 0)
-const char *path = "/share/slides/slides-%d.bmp";
 
 static SDL_Surface *slide = NULL;
-static int cur = 0;
 
 void render() {
   if (slide != NULL) {
