6c2489e4179efdc6354b855eb4c6bd1b622886f4
tracer-ics2020 <tracer@njuics.org>
2020-11-30 11:59:58 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 4d072052e06f4130b0dbdab8a218d6f7     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  11:59:58 up  5:46,  8 users,  load average: 0.12, 0.22, 0.18

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -3,10 +3,39 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define W 400
+#define H 300
+
+// USAGE:
+//   j/down - page down
+//   k/up - page up
+//   gg - first page
+
+// number of slides
+const int N = 53;
+// slides path pattern (starts from 0)
+const char *path = "/share/slides/slides-%d.bmp";
+
 static SDL_Surface *slide = NULL;
+static int cur = 0;
+
+void render() {
+  if (slide != NULL) {
+    SDL_FreeSurface(slide);
+  }
+  char fname[256];
+  sprintf(fname, path, cur);
+  slide = SDL_LoadBMP(fname);
+  assert(slide);
+  SDL_UpdateRect(slide, 0, 0, 0, 0);
+}
 
 int main() {
-  if (slide != NULL) printf("yzhsb");
-  else printf("sd");
+  SDL_Init(0);
+  SDL_Surface *screen = SDL_SetVideoMode(W, H, 32, SDL_HWSURFACE);
+
+  int rep = 0, g = 0;
+  
+  render();
   return 0;
 }
