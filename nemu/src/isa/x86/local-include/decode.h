5c1699be25f85563fcf48624df0bf975ba3f7598
tracer-ics2020 <tracer@njuics.org>
2020-11-30 11:26:13 -0500

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 4d072052e06f4130b0dbdab8a218d6f7     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  11:26:13 up  5:13,  8 users,  load average: 0.32, 0.18, 0.19

--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -100,7 +100,7 @@ static inline def_DopHelper(O) {
     rtl_lm(s, &op->val, s->isa.mbase, s->isa.moff, op->width);
     op->preg = &op->val;
   }
-  print_Dop(op->str, OP_STR_SIZE, "0x%x", op->val);
+  print_Dop(op->str, OP_STR_SIZE, "0x%x", s->isa.moff);
 }
 
 /* Eb <- Gb
