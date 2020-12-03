c038e330e13adf3c84c550ae9ae16e8d46d2bd10
tracer-ics2020 <tracer@njuics.org>
2020-12-03 13:20:07 -0500

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: a88fef86c5514c07b38497a66aa80bb8     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:20:07 up  4:23,  8 users,  load average: 0.04, 0.16, 0.51

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -12,6 +12,7 @@ static inline def_EHelper(test) {
 static inline def_EHelper(and) {
   rtl_and(s, s0, ddest, dsrc1);
   operand_write(s, id_dest, s0);
+  printf("%d %d %d\n", *ddest, *s0, *dsrc1);
   rtl_set_CF(s, rz);
   rtl_set_OF(s, rz);
   rtl_update_ZFSF(s, ddest, s->dest.width);
