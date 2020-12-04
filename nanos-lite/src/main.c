bd442a77866e9f99a9bceb5c3ad1095bed7405d0
tracer-ics2020 <tracer@njuics.org>
2020-12-04 08:25:04 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 8447517f6a9c48269a790a774c2a43a2     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:25:04 up 16 min,  3 users,  load average: 0.69, 0.48, 0.30

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -7,6 +7,7 @@ void init_irq(void);
 void init_fs(void);
 void init_proc(void);
 
+extern uint8_t _data;
 int main() {
   extern const char logo[];
   printf("%s", logo);
@@ -26,7 +27,7 @@ int main() {
 #endif
 
   init_fs();
-
+printf("%08x\n", _data);
   init_proc();
 
   Log("Finish initialization");
