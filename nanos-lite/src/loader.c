3b1a8efd993a5a3f8de43e5d3fe5d9f1496abb3d
tracer-ics2020 <tracer@njuics.org>
2020-12-04 14:05:00 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: c6fe199653ef40ec80289974b8fbd51a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  14:05:00 up  1:18,  5 users,  load average: 0.27, 0.42, 0.61

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -25,7 +25,10 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       if (filename == NULL) head_addr = 0;
 	  else {
 	     int fd = fs_open(filename, 0, 0);
-		 assert(fd != -1);
+		 if (fd == -1) {
+			printf("Can't find the file!\n'");
+			return 0;	 
+		}
 		 head_addr = get_head(fd);
 		 fs_close(fd);			  
 	  }
@@ -34,7 +37,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 	  if (elf_head.e_machine != EXPECT_TYPE) {
 		uint32_t tep = EXPECT_TYPE;   
 		printf("%08x %08x\n", elf_head.e_machine, tep);
-		panic("The ISA counldn't be matched!");	  
+		printf("The ISA counldn't be matched!\n");
+		return 0;	  
 	  }
 	  uintptr_t addr = elf_head.e_entry;
 	  Elf_Phdr *phdr = (Elf_Phdr *)malloc(sizeof(Elf_Phdr) * elf_head.e_phnum);
@@ -51,6 +55,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 }
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
+  if (entry == 0)return;
   Log("Jump to entry = %p", entry);
   ((void(*)())entry) ();
 }
