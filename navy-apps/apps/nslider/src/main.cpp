9b62ae4108566dea9d92d7dc35e804c05cebba34
tracer-ics2020 <tracer@njuics.org>
2020-12-04 22:30:34 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: c6fe199653ef40ec80289974b8fbd51a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:30:34 up  2:23,  4 users,  load average: 1.04, 1.10, 0.90

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -2,7 +2,7 @@
 #include <SDL_bmp.h>
 #include <stdio.h>
 #include <assert.h>
-
+#include<stdlib.h>
 #define W 400
 #define H 300
 
@@ -79,6 +79,7 @@ int main() {
             rep = 0; g = 0;
           }
           break;
+	   case SDLK_ESCAPE: exit(0); break;  
       }
     }
   }
