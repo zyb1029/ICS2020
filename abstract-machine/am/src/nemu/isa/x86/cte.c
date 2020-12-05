f39b848311753a02c2022001dca8794a197a85f8
tracer-ics2020 <tracer@njuics.org>
2020-12-05 10:49:28 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: c6fe199653ef40ec80289974b8fbd51a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:49:28 up 10:32,  4 users,  load average: 0.58, 0.43, 0.43

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -58,9 +58,9 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 
 Context* kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *c = (Context *)kstack.end - 1;
-  intptr_t *loc = (intptr_t *)(&(c->eflags) + 2);
-  *loc = (intptr_t) arg;
-  printf("%s\n", (char *) arg);return c;
+  char *loc = (char *)(&(c->eflags) + 2);
+  *loc = (intptr_t)((char *)arg);
+ // printf("%d %s\n", (char *) arg);return c;
   c->cr3 = 0;
   c->edi = 0, c->esi = 0, c->ebp = 0, c->esp = 0, c->ebx = 0, c->edx = 0;
   c->ecx = 0, c->eax = 0;
