efa376fae0b109b16dcb9b52d2418c235f86d9a1
tracer-ics2020 <tracer@njuics.org>
2020-12-04 22:22:25 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: c6fe199653ef40ec80289974b8fbd51a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:22:25 up  2:15,  4 users,  load average: 0.55, 0.68, 0.74

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -142,7 +142,7 @@ char handle_key(const char *buf) {
 char handle_key(SDL_Event *ev) {
   static int shift = 0;
   int key = ev->key.keysym.sym;
-  if (key == SDL_ESCAPE) exit(0);
+  if (key == SDLK_ESCAPE) exit(0);
   if (key == SDLK_LSHIFT || key == SDLK_RSHIFT) { shift ^= 1; return '\0'; }
 
   if (ev->type == SDL_KEYDOWN) {
