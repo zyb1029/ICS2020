b3dcb280082b628075c1d0e9f0dbaabaf71bb92e
tracer-ics2020 <tracer@njuics.org>
2020-12-05 10:33:10 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: c6fe199653ef40ec80289974b8fbd51a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:33:10 up 10:16,  4 users,  load average: 1.13, 0.64, 0.49

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,6 +25,7 @@ void context_kload(PCB * pcb, void* loc, void* arg) {
 	area.start = pcb;
 	area.end = (char *)pcb + sizeof(PCB);
 	printf("%p\n", arg);
+	while(1);
 	pcb -> cp = kcontext(area, loc, arg);
 }
 
