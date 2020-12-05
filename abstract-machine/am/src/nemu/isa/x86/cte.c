fd918695fafb763b54fe614dbc6b7ee5b774e2fc
tracer-ics2020 <tracer@njuics.org>
2020-12-05 10:28:09 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: c6fe199653ef40ec80289974b8fbd51a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:28:09 up 10:11,  4 users,  load average: 0.33, 0.38, 0.38

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -61,7 +61,6 @@ Context* kcontext(Area kstack, void (*entry)(void *), void *arg) {
   intptr_t *loc = (intptr_t *)(&(c->eflags) + 2);
   *loc = (intptr_t) arg;
   c->cr3 = 0;
-  return c;
   c->edi = 0, c->esi = 0, c->ebp = 0, c->esp = 0, c->ebx = 0, c->edx = 0;
   c->ecx = 0, c->eax = 0;
   c->irq = 0x81;
