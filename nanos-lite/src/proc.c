084f44773abfcff8e1d27588a3eb1eb48d0c3d2e
tracer-ics2020 <tracer@njuics.org>
2020-12-05 10:31:05 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: c6fe199653ef40ec80289974b8fbd51a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:31:05 up 10:14,  4 users,  load average: 0.33, 0.42, 0.40

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,7 +29,7 @@ void context_kload(PCB * pcb, void* loc, void* arg) {
 
 
 void init_proc() {
-  context_kload(&pcb[0], (void *)hello_fun, "-b");
+  context_kload(&pcb[0], (void *)hello_fun, (void *)1);
   switch_boot_pcb();
  /* 
   Log("Initializing processes...");
