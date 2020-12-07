d557e9dc2dd241b0c476562bbb2b8f01a933fe09
tracer-ics2020 <tracer@njuics.org>
2020-12-07 12:28:29 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 1bc5395103f04856bc09b8506a2daa9c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:28:29 up 32 min,  3 users,  load average: 0.17, 0.16, 0.17

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,7 +32,7 @@ void context_uload(PCB * pcb, const char* filename, char *const argv[], char *co
 	area.end = heap.end;
 	pcb -> cp = ucontext(NULL, area, (void *)loader(NULL, filename));
 	int argc = 0;
-	printf("%c\n", argv[1][0]);
+	if (argv[2] == NULL) printf("3232");
 	for (int i = 0; ;i++)
 		if(argv[argc] != NULL) argc++;
 	printf("%d\n", argc);
