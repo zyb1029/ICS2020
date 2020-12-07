22b23bc6137756e03b7e7085ccb5ff5c17477c06
tracer-ics2020 <tracer@njuics.org>
2020-12-07 14:43:08 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 1bc5395103f04856bc09b8506a2daa9c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  14:43:08 up  2:44,  5 users,  load average: 0.86, 0.61, 0.51

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,21 +5,21 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  int* tep;
-  tep = (int *)args[0];
-  int argc = *tep;
+  uintptr_t* tep;
+  tep = (uintptr_t *)args[0];
+  int argc = (int)(*tep);
 
   char **argv, **envp;
 
-  char *tep1;
-  tep1 = (char *) args[1];
-  argv = (char **)tep1;
+  uintptr_t *tep1;
+  tep1 = (uintptr_t *) args[1];
+  argv = (char **)(*tep1);
 
-  char *tep2;
-  tep2 = (char *) args[2];
-  envp = (char **)tep2;
+  uintptr_t *tep2;
+  tep2 = (uintptr_t *) args[2];
+  envp = (char **)(*tep2);
 
-  printf("%d %s %s\n", args, argv[0], envp[0]);
+  printf("%d %s %s\n", argc, argv[0], envp[0]);
   environ = envp;
   while(1);
   exit(main(argc, argv, envp));
