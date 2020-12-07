0577478683cfb5ffee11d55b227c4285076773ae
tracer-ics2020 <tracer@njuics.org>
2020-12-07 13:07:54 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 1bc5395103f04856bc09b8506a2daa9c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:07:54 up  1:11,  3 users,  load average: 0.30, 0.26, 0.20

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -71,5 +71,6 @@ void init_proc() {
 Context* schedule(Context *prev) {
   current -> cp = prev;
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  assert(current->cp == NULL);
   return current -> cp;
 }
