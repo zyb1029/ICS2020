d31706e4ecae8856b4e34f733e0e0de0583bc4ac
tracer-ics2020 <tracer@njuics.org>
2020-12-08 00:39:37 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 1bc5395103f04856bc09b8506a2daa9c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  00:39:38 up  5:45,  5 users,  load average: 0.28, 0.27, 0.26

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -37,16 +37,14 @@ void context_uload(PCB * pcb, const char* filename, char *const argv[], char *co
 	pcb -> cp = ucontext(&(pcb->as), area,(void *)loader(NULL, filename));
 	uintptr_t *loc;
 	loc = ((uintptr_t *)heap.end - 1);
-	printf("%p\n", loc);
-	*loc = 1;
 	pcb -> cp -> GPRx = (uintptr_t)((uintptr_t *)heap.end - 1);
 	for (int i = 0; ;i++)
 		if (argv[argc[tot]] != NULL) argc[tot]++;
 		else break;
 	p[tot][0] = (uintptr_t )(&argc[tot]);
 	p[tot][1] = (uintptr_t )(argv);
-	p[tot][2] = (uintptr_t )(envp);return;
-	pcb -> cp -> GPRx = (uintptr_t)p[tot];
+	p[tot][2] = (uintptr_t )(envp);
+	*loc = (uintptr_t)p[tot];
 	tot++; 
 }
 
