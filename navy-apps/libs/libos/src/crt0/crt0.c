09b650223d61ef4d4f946d9c6475e86837c20f98
tracer-ics2020 <tracer@njuics.org>
2020-12-08 01:45:41 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 1bc5395103f04856bc09b8506a2daa9c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  01:45:41 up  6:49,  5 users,  load average: 2.09, 1.44, 0.90

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -11,25 +11,20 @@ void call_main(uintptr_t *args) {
   int now = 1;
   for (int i = now; ; i++){
 	  argv[i] = (char *)args[i];
-	  printf("%s\n", argv[i]);
 	  if(argv[i] == NULL)break;
 	  else now++;
+	  printf("%s\n", argv[i]);
   }
   now = now + 1;
 printf("\n");
   for (int i = now; ; i++){
 	  envp[i] = (char *)args[i];
-	  printf("%s\n", envp[i]);
 	  if(argv[i] == NULL)break;
 	  else now++;
+	  printf("%s\n", envp[i]);
   }
 
-  while(1);
-  argv = (char **) args[1];
-  envp = (char **) args[2];
   environ = envp;
-  char **empty = {NULL};
-  environ = empty;
-  exit(main(0, empty, empty));
+  exit(main(argc, argv, envp));
   assert(0);
 }
