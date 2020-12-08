6c81ad66594c08bb8a740a104519bc8068fd1103
tracer-ics2020 <tracer@njuics.org>
2020-12-08 02:38:31 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 1bc5395103f04856bc09b8506a2daa9c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  02:38:31 up  7:34,  3 users,  load average: 2.25, 2.37, 2.00

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,7 +15,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-	yield();
+//	yield();
   char *p = (char *)buf;
   for (int i = 0; i < len; i++) {
 	  putch(*(p + i));
@@ -24,7 +24,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-	yield();
+//	yield();
   char tep[32];
   bool has_kbd = io_read(AM_INPUT_CONFIG).present;
   if (has_kbd) {
@@ -41,7 +41,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-	yield();
+//	yield();
   int w = io_read(AM_GPU_CONFIG).width;
   int h = io_read(AM_GPU_CONFIG).height;
   char tep[32];
@@ -55,7 +55,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-	yield();
+//	yield();
   int y = (offset >> 2) / io_read(AM_GPU_CONFIG).width;
   int x = (offset >> 2) - y * io_read(AM_GPU_CONFIG).width;
   uint32_t * p = (uint32_t *)buf; 
