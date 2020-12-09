b3acd057729bb127b6edcc5f8defb7a27cc412d1
tracer-ics2020 <tracer@njuics.org>
2020-12-09 10:53:15 -0500

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 56cff00e55b04f129167a88c15683742     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:53:15 up  6:45,  4 users,  load average: 0.17, 0.08, 0.08

--- nemu/src/isa/x86/exec/system.h
+++ nemu/src/isa/x86/exec/system.h
@@ -14,6 +14,7 @@ static inline def_EHelper(mov_r2cr) {
   rtl_lr(s, s0, id_src1->reg, 4);
   if (id_dest ->reg == 3) cpu.cr3 = *s0;
   else cpu.cr0 = *s0;
+  printf("%8x %8d\n", cpu.cr3, cpu.cr0);
   print_asm("movl %%%s,%%cr%d", reg_name(id_src1->reg, 4), id_dest->reg);
 }
 
