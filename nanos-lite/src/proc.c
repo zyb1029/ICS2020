9f2ec2637d8f39087b0a826ef29ce59c62625853
tracer-ics2020 <tracer@njuics.org>
2020-12-08 19:44:55 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 1bc5395103f04856bc09b8506a2daa9c     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  19:44:55 up 14:15,  4 users,  load average: 1.22, 1.22, 1.18

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,7 +32,6 @@ void context_uload(PCB * pcb, const char* filename, char *const argv[], char *co
 
 	uintptr_t *loc;
 	loc = ((uintptr_t *)new_page(4) - 1);
-	printf("%p\n", loc);
     int env_argc = 0;
 	for (int i = 0; ; i++)
 		if (envp[env_argc] == NULL) break;
@@ -86,8 +85,8 @@ void init_proc() {
 
 }
 
-Context* schedule(Context *prev) {
+Context* schedule(Context *prev) {assert(0);
   current -> cp = prev;
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  current = (current == &pcb[0] ? &pcb[0] : &pcb[0]);
   return current -> cp;
 }
