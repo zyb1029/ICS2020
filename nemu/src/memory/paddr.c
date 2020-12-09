a80c473a89101cec16d893e3f4a8d12fd0a27e9c
tracer-ics2020 <tracer@njuics.org>
2020-12-09 14:09:29 -0500

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 56cff00e55b04f129167a88c15683742     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  14:09:29 up  9:51,  4 users,  load average: 0.47, 0.24, 0.18

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -69,7 +69,7 @@ word_t vaddr_mmu_read(vaddr_t addr, int len, int type) {
 	  else {
 		word_t ans = 0, tep = 0;
 		for (int i = 0; i < len; i++) {
-			pg_base = isa_mmu_translate(addr,type, 1);
+			pg_base = isa_mmu_translate(addr + i,type, 1);
 			tep = paddr_read(addr, 1);
 			printf("%x ", tep);
 			ans = ans + (tep << (8 * i));
