6c8ef2189d69a1bfbd653de2812d30b8af0bc43c
tracer-ics2020 <tracer@njuics.org>
2020-12-10 10:23:31 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 56cff00e55b04f129167a88c15683742     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:23:31 up 18:33, 10 users,  load average: 0.34, 0.56, 0.87

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -92,7 +92,7 @@ static char *envp[] = {"PATH=/bin/:/usr/bin/", NULL};
 void init_proc() {
 
 //  context_kload(&pcb[1], (void *)hello_fun, (void *)"-bb");
-  context_uload(&pcb[0], "/bin/pal", argv, envp);
+  context_uload(&pcb[0], "/bin/nterm", argv, envp);
   switch_boot_pcb();
   
   Log("Initializing processes...");
