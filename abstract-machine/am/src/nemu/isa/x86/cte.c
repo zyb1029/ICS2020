3a7f2eb5c85462a2a5e7341c92e1c260946c26e3
tracer-ics2020 <tracer@njuics.org>
2020-12-22 14:37:23 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 7a8ca06a438e4cd9b3264767b28f6bd0     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  14:37:23 up  4:51,  5 users,  load average: 0.42, 0.46, 0.34

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -15,7 +15,7 @@ void __am_vecnull();
 
 
 Context* __am_irq_handle(Context *c) {
-	printf("eax %x\n", c->ebx);
+	printf("eax %x\n", c->esp);
   __am_get_cur_as(c);
   if (user_handler) {
     Event ev = {0};
@@ -32,7 +32,7 @@ Context* __am_irq_handle(Context *c) {
     assert(c != NULL);
   }
   __am_switch(c);
-	printf("eax %x\n", c->ebx);
+	printf("eax %x\n", c->esp);
 
   return c;
 }
