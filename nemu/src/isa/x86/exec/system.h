33f0c726c49f4bdf5361a019cdebdb6976dc9e82
tracer-ics2020 <tracer@njuics.org>
2020-12-23 08:44:49 -0500

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 7a8ca06a438e4cd9b3264767b28f6bd0     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:44:49 up 14:21,  8 users,  load average: 1.33, 1.34, 1.23

--- nemu/src/isa/x86/exec/system.h
+++ nemu/src/isa/x86/exec/system.h
@@ -108,7 +108,7 @@ static inline def_EHelper(iret) {
 	 if ((cpu.cs & 0x3) == 0x3)
 		  rtl_sm(s, s1, 4, s0, 4);
 	 if (tep) cpu.esp = tep;
-	 printf("touch iret\n");
+	 printf("%x\n", tep);
   print_asm("iret");
 
 #ifndef __DIFF_REF_NEMU__
