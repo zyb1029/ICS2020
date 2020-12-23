d40cfd9c24945e18852468917e61475fc0078417
tracer-ics2020 <tracer@njuics.org>
2020-12-22 22:41:41 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 7a8ca06a438e4cd9b3264767b28f6bd0     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:41:41 up  8:23,  5 users,  load average: 0.34, 0.20, 0.37

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -81,7 +81,7 @@ void context_uload(PCB * pcb, const char* filename, char *const argv[], char *co
 			else {
 				argv2[argc] = malloc(sizeof(argv[argc]) + 1);
 				strcpy(argv2[argc], argv[argc]);
-				printf("%x %s %d\n", loc, argv2[0], argc);
+				printf("%x %s %d\n", loc, argv2[argc], argc);
 				argc++;
 			}
 		for (int i = argc; i >= 0; i--){
