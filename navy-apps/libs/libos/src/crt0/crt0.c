87119cc1954b22aac9fac5ae3d7565f2652153b5
tracer-ics2020 <tracer@njuics.org>
2020-12-23 04:16:41 -0500

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 7a8ca06a438e4cd9b3264767b28f6bd0     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  04:16:41 up 11:21,  9 users,  load average: 0.21, 0.22, 0.26

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -4,12 +4,12 @@
 #include <string.h>
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
-static char *envp[64];
 void call_main(uintptr_t *args) {
   char *argv[64];
+static char *envp[64];
   int argc = (int)args[0];
   int now = 1;
-  printf("%x  8  ,\n", args);
+  printf("%x  8  ,\n", argc);
   for (int i = now, j = 0; ; i++, j++){
 	  argv[j] = (char *)args[i];
 	  if(argv[j] == NULL)break;
