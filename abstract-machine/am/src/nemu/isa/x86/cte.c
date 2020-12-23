4c025f9ad87287dcd48761853536123a65b2ae4e
tracer-ics2020 <tracer@njuics.org>
2020-12-22 20:38:34 -0500

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 7a8ca06a438e4cd9b3264767b28f6bd0     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  20:38:35 up  6:33,  6 users,  load average: 0.56, 0.47, 0.28

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -75,9 +75,8 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 Context* kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *c = (Context *)kstack.end - 2;
   intptr_t* loc;
-  loc = (intptr_t *)(&(c->eflags) + 2);
+  loc = (intptr_t *)(&(c->ss3) + 2);
   *loc = (intptr_t)arg;
-  printf("%x\n",arg);
   c->cr3 = 0;
   c->edi = 0, c->esi = 0, c->ebp = 0, c->esp = 0, c->ebx = 0, c->edx = 0;
   c->ecx = 0, c->eax = 0;
--- nemu/src/isa/x86/exec/system.h
+++ nemu/src/isa/x86/exec/system.h
@@ -106,7 +106,7 @@ static inline def_EHelper(iret) {
 	 rtl_li(s, s0, cpu.esp);
 	 if ((cpu.cs & 0x3) == 0x3)
 		  rtl_sm(s, s1, 4, s0, 4);
-	 if (tep) cpu.esp = tep, printf("23232\n");
+	 if (tep) cpu.esp = tep;
   print_asm("iret");
 
 #ifndef __DIFF_REF_NEMU__
