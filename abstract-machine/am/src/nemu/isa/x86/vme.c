9f26bfa74196f60b22b97548989f8cb21a529665
tracer-ics2020 <tracer@njuics.org>
2020-12-23 10:28:47 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 7a8ca06a438e4cd9b3264767b28f6bd0     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:28:48 up 15:27,  9 users,  load average: 1.52, 1.53, 1.40

--- abstract-machine/am/src/nemu/isa/x86/vme.c
+++ abstract-machine/am/src/nemu/isa/x86/vme.c
@@ -38,6 +38,13 @@ void protect(AddrSpace *as) {
   as->pgsize = PGSIZE;
   // map kernel space
   memcpy(updir, kas.ptr, PGSIZE);
+    if ((uintptr_t)updir  == 0x2680000) {
+		printf("%x\n", updir);
+		uint32_t *loc;
+		loc = (uint32_t *)updir;
+		loc = loc + (((0x101b5e) & ~0x3fffff) >> 22);
+		printf("%x\n", *loc);
+	}
 }
 
 void unprotect(AddrSpace *as) {
