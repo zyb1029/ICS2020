076a26377f7681f3ef1dbd65c087503921f9eb4a
tracer-ics2020 <tracer@njuics.org>
2020-12-23 06:38:38 -0500

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 7a8ca06a438e4cd9b3264767b28f6bd0     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  06:38:38 up 12:30,  7 users,  load average: 1.31, 1.17, 1.07

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -18,7 +18,6 @@ Context* __am_irq_handle(Context *c) {
   __am_get_cur_as(c);
   if (user_handler) {
     Event ev = {0};
-	printf("%x\n", c->irq);
     switch (c->irq) {
 	  case 0x20:
 		ev.event = EVENT_IRQ_TIMER;break;
