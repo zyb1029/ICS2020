a8dbb377f8e0356ced938adad751bcd999f28d2a
tracer-ics2020 <tracer@njuics.org>
2020-12-23 04:24:13 -0500

    >  compile 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 7a8ca06a438e4cd9b3264767b28f6bd0     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  04:24:13 up 11:29,  9 users,  load average: 0.34, 0.29, 0.28

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -16,6 +16,7 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
 	vaddr_t tep = cpu.esp;
 	printf("instr :%x\n", cpu.esp);
 	if (ksp != 0) cpu.esp = ksp;
+	printf("instr2:%x\n", cpu.esp);
 	rtl_lm(s, s0, s1, 8, 4);
 	rtl_li(s, s1, *s0);
 	rtl_push(s, s1);
