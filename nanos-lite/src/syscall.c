f25d654cbb9aa2f273ff129cca4a71a1b1a75c1c
tracer-ics2020 <tracer@njuics.org>
2020-12-23 08:52:16 -0500

    >  run 191220186    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: a6b30bf55e04419c92f74dfd76fd6b7d            Boot ID: 7a8ca06a438e4cd9b3264767b28f6bd0     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:52:16 up 14:28,  8 users,  load average: 1.21, 1.33, 1.27

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -74,6 +74,7 @@ void do_syscall(Context *c) {
 		else {
 			ex_flag = true;
 			context_uload(current, (char *)c->GPR2,(char **)c->GPR3, (char **)c->GPR4);
+	printf("schdule: %x\n", current->cp->eax);
 			switch_boot_pcb();
 			c->GPRx = -1;
 			printf("switch\n");
